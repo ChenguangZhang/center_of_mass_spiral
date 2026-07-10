f9ef679c45bba29d4df0fae152b4dc9eeacfb900
ChenguangZhang <hi.chenguang@gmail.com>
2026-07-09 22:47:08 -0400

    truly frozen

--- vertex_list.py
+++ vertex_list.py
@@ -8,3 +8,9 @@ class VertexList:
     vertices: np.ndarray
     is_closed: bool
     is_discrete: bool
+
+    def __post_init__(self):
+        vertices_copy = self.vertices.copy()
+        vertices_copy.flags.writeable = False
+        # Use object.__setattr__ to bypass the frozen=True restriction
+        object.__setattr__(self, 'vertices', vertices_copy)
