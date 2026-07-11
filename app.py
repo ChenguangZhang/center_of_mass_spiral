60e1c259e610fc3b7fcb3f263bd685256c308ccd
ChenguangZhang <hi.chenguang@gmail.com>
2026-07-11 15:21:23 -0400

    just show the figure, do not save

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from plotter import plot_polysegment
 import operations
 import matplotlib.pyplot as plt
 
-shapes = [Ellipse(1, 2, 100), NGon(6, 1), NGon(5, 1), NGon(
+shapes = [Ellipse(1, 2, 100), NGon(6, 1), NGon(
     4, 1), NGon(3, 1), Parabola(0, 1, 100), Flower(1, 0.1, 10, 100)]
 
-for shape in shapes:
+plt.figure(figsize=(12, 8))
+for i, shape in enumerate(shapes):
     vl = shape.get_vertex_list()
     vl = operations.repeat(vl, 10)
 
@@ -16,7 +17,10 @@ for shape in shapes:
 
     cx, cy = get_com_spiral(pseg)
 
-    plot_polysegment(pseg, color='black', linewidth=1, alpha=0.5)
+    plt.subplot(2, 3, i + 1)
+    plot_polysegment(pseg, ax=plt.gca(), color='black', linewidth=1, alpha=0.5)
     plt.plot(cx, cy, 'k-')
     plt.axis('equal')
-    plt.savefig(f'fig_{vl.name}.png', dpi=300)
+
+plt.tight_layout()
+plt.show()
