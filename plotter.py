67f0fae6215cfd10249ccf8c642d4dbe3e9c7475
ChenguangZhang <hi.chenguang@gmail.com>
2026-07-11 14:38:28 -0400

    rename

--- plotter.py
+++ plotter.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 def plot_polysegment(pseg, ax=None, **kwargs):
     if ax is None:
         fig, ax = plt.subplots()
-    for segment in pseg:
-        x = [segment.x1, segment.x2]
-        y = [segment.y1, segment.y2]
+    for seg in pseg:
+        x = [seg.x1, seg.x2]
+        y = [seg.y1, seg.y2]
         ax.plot(x, y, **kwargs)
     return ax
