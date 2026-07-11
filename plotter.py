ee4817a1b5bdf6b7d19e1cd4e3e684930bcb842e
ChenguangZhang <hi.chenguang@gmail.com>
2026-07-10 23:00:21 -0400

    rename to pseg and pythonic iter

--- plotter.py
+++ plotter.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
 
-def plot_polysegment(polysegment, ax=None, **kwargs):
+def plot_polysegment(pseg, ax=None, **kwargs):
     if ax is None:
         fig, ax = plt.subplots()
-    for segment in polysegment.segments:
+    for segment in pseg:
         x = [segment.x1, segment.x2]
         y = [segment.y1, segment.y2]
         ax.plot(x, y, **kwargs)
