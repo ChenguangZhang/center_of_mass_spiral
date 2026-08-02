78cbd16deb67a8b8bf360650ff76bf4bfeff110a
ChenguangZhang <hi.chenguang@gmail.com>
2026-08-01 21:52:07 -0400

    quick benchmark

--- src/center_of_mass_spiral/plotter.py
+++ src/center_of_mass_spiral/plotter.py
@@ -3,6 +3,8 @@ import numpy as np
 from .poly_segment import PolySegment
 
 
+# from .perf_tools import timing_decorator
+# @timing_decorator # disabled after profiling
 def plot_polysegment(pseg: PolySegment, ax=None, **kwargs):
     if ax is None:
         fig, ax = plt.subplots()
@@ -10,6 +12,14 @@ def plot_polysegment(pseg: PolySegment, ax=None, **kwargs):
     if vl.num_repeat == 1:
         V = vl.vertices
     else:
+        # Math is a bit tricky here. Consider a square. It is defined by 5 vertices
+        # (the first and last overlap). When repeating, we drop the last vertex,
+        # repeat, and add it back.
+        # For example, after repeating 3 times, the number of vertices is
+        #                              (5-1)*3+1=13.
+        # To recover the vertex count in the first loop, we need to reverse the above formula:
+        #                         (len(vl) - 1)/n_repeat + 1.
+        # which is the one used below.
         n_first_loop = (len(vl.vertices) - 1) // vl.num_repeat + 1
         V = vl.vertices[:n_first_loop, :]
 
